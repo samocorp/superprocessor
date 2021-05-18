56c6e94e27ab102be8b515395886f38c0ed3a737
Ron Nathaniel <rnathaniel7@gmail.com>
2021-05-17 20:56:54 -0400

    feat(main): added annotations

--- superprocessor/main.py
+++ superprocessor/main.py
@@ -4,8 +4,20 @@ from typing import (
     Union, Tuple
 )
 
+"""
+Samo c 2021
+Author: Samoto
+"""
+
 
 def cmd(* args: str) -> Tuple[str, Union[str, None]]:
+    """
+
+    :param args: list of strings --
+        to be concatenated into stmt
+    :return: tuple --
+        of length 2: std (output, error)
+    """
 
     if not args:
         return tuple()
@@ -22,5 +34,3 @@ def cmd(* args: str) -> Tuple[str, Union[str, None]]:
                   text=True)
 
     return resp.stdout, resp.stderr or None
-
-
