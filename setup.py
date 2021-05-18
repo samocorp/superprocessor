96ef95e8286b6d748e9f7d64f8afe645a8694c2d
Ron Nathaniel <rnathaniel7@gmail.com>
2021-05-18 19:56:51 -0400

    fix(setup): exportable bug of dist/non-dist

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     version="0.0.1",
     author="samo",
     author_email="thesamogroup@gmail.com",
-    description="Super Processor - the perfect wrapper for subprocess.run",
+    description="SuperProcessor: the perfect wrapper for subprocess.run",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/samocorp/superprocessor",
@@ -21,7 +21,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "superprocessor"},
-    packages=setuptools.find_packages(where="superprocessor"),
+    packages=setuptools.find_packages(exclude=('tests', 'examples')),
     python_requires=">=3.6",
 )
