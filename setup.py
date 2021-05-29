cf48a23d17f3a77d9b232b9baeef17ba9a5c0c7c
Ron Nathaniel <rnathaniel7@gmail.com>
2021-05-28 21:50:39 -0400

    RELEASE: v0.2.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="superprocessor",
-    version='0.1.2',
+    version='0.2.0',
     author="samo",
     author_email="thesamogroup@gmail.com",
     description="SuperProcessor: the perfect wrapper for subprocess.run",
